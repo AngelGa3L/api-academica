0a1b788c4bc340989b06af2e8032f7c0a93b274f
AngelGa3L <angelgaelguevara@gmail.com>
2025-06-20 22:18:24 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+import express from 'express'
+import cors from 'cors'
+
+const app = express()
+const port = 44221
+
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  }));
+
+app.use(express.json())
+
+app.listen(port, () => {
+    console.log(`App listening on port ${port}`)
+})
